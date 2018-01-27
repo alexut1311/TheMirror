8d2a90138966c7f390c2e1583695f93e383a2def
Sebastian <sebastianboteanu@gmail.com>
2018-01-27 22:17:34 +0200

    Add graphics

--- script.js
+++ script.js
@@ -5,17 +5,29 @@ function win() {
     if ($(window).width() >= 1320) {
 
         $(".grid").css("width", "1320px");
+        $("#first").css("width", "1300px");
+        $("#secondP").css("font-size", "17px");
+
     }
     if ($(window).width() < 1320) {
         $(".grid").css("width", "1100px");
+        $("#first").css("width", "1080px");
+        $("#secondP").css("font-size", "15px");
+
     }
 
 
+
 }
-//630,535,420,300,190,90
 
 $(document).ready(function () {
     "use strict";
+    win();
+    $('.grid').masonry({
+        columnWidth: 220,
+        itemSelector: '.grid-item'
+    });
+
     var pos = [630, 535, 420, 300, 190, 90];
     $("li").click(function () {
         $("#arrow").css("right", pos[$(this).index()]);
