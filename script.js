69cb881cea0a3e1bdfc52600b5a1610c9795d315
Sebastian <sebastianboteanu@gmail.com>
2018-01-29 13:07:42 +0200

    adding coments

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 /*global $, jQuery, alert*/
 /*jslint plusplus: true */
+//Am folosit jquery datorita usurintei de folosire
 function win() {
     "use strict";
+    // gridul si bara de stiri isi schimba dimensiunea daca fereastra este mai mica de 1320px.textul in bara
+    //de stiri se modifica la redimensionare
     if ($(window).width() >= 1320) {
 
         $(".grid").css("width", "1320px");
@@ -25,22 +28,23 @@ $(document).ready(function () {
     win();
 
 
-    $('.grid').masonry({
+    $('.grid').masonry({ //initializarea gridului
         columnWidth: 220,
         itemSelector: '.grid-item'
     });
 
-    var pos = [630, 535, 420, 300, 190, 90],
-        dots = [$("#dot1"), $("#dot2"), $("#dot3")],
-        text = [$("#dot1"), $("#dot2"), $("#dot3")],
+    var pos = [630, 535, 420, 300, 190, 90], //pozitia sagetii
+        dots = [$("#dot1"), $("#dot2"), $("#dot3")], // puncte pentru indicarea indexului
+        text = [$("#dot1"), $("#dot2"), $("#dot3")], //lista cu stirile
         i = 0;
 
-    $("p.secondP").each(function () {
+    $("p.secondP").each(function () { // creearea listei cu stiri
         text[i] = $(this);
 
         i++;
 
     });
+    //cand se da click pe un punct, stirea se schimba in functie de index si punctul devine alb, restul avand doar o margine alba
     dots[0].click(function () {
         dots[0].attr("src", "dotFull.png");
         dots[1].attr("src", "dotHollow.png");
@@ -79,10 +83,10 @@ $(document).ready(function () {
     });
 
     $("li").click(function () {
-        $("#arrow").css("right", pos[$(this).index()]);
+        $("#arrow").css("right", pos[$(this).index()]); // schimbarea pozitiei sagetii in functie de meniul apasat
     });
 });
-$(window).resize(function () {
+$(window).resize(function () { // functie apelata in momentul cand fereastra se redimensioneaza
     "use strict";
     win();
 });
